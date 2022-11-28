7f9cde0b711d1c089c81a43062d62316cda01969
JIYOUNG PARK <ziy913@naver.com>
2022-11-29 02:20:05 +0900

    로그인 화면 구현 클릭어쩌구 아직 안 함

--- front/instagram_clone/src/home/Home.java
+++ front/instagram_clone/src/home/Home.java
@@ -1,14 +1,10 @@
 package home;
 
-import User.EachUserBoard;
-
 import javax.swing.*;
 import javax.swing.border.Border;
 import javax.swing.border.CompoundBorder;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
@@ -16,7 +12,7 @@ public class Home extends JFrame {
 
     Toolkit toolkit = Toolkit.getDefaultToolkit();
     Dimension screenSize = toolkit.getScreenSize();
-    private static ImageIcon ICON = new ImageIcon("image/instagram_circle.png");
+    private static ImageIcon ICON = new ImageIcon("front/instagram_clone/image/instagram_circle.png");
     int allHeight = screenSize.height - 100;
 
     JPanel menuPanel = new JPanel(new BorderLayout());
@@ -27,23 +23,23 @@ public class Home extends JFrame {
     JPanel menuMainPanel = new JPanel();
     JPanel menuMorePanel = new JPanel(new BorderLayout());
 
-    ImageIcon menuLogo = new ImageIcon("image/menu_logo.png");
-    ImageIcon menuHome = new ImageIcon("image/menu_home.png");
-    ImageIcon menuSearch = new ImageIcon("image/menu_search.png");
-    ImageIcon menuDiscover = new ImageIcon("image/menu_discover.png");
-    ImageIcon menuMessage = new ImageIcon("image/menu_message.png");
-    ImageIcon menuNotice = new ImageIcon("image/menu_notice.png");
-    ImageIcon menuUpload = new ImageIcon("image/menu_upload.png");
-    ImageIcon menuProfil = new ImageIcon("image/harry_potter_profil_circle.png");
-    ImageIcon menuMore = new ImageIcon("image/menu_more.png");
+    ImageIcon menuLogo = new ImageIcon("front/instagram_clone/image/menu_logo.png");
+    ImageIcon menuHome = new ImageIcon("front/instagram_clone/image/menu_home.png");
+    ImageIcon menuSearch = new ImageIcon("front/instagram_clone/image/menu_search.png");
+    ImageIcon menuDiscover = new ImageIcon("front/instagram_clone/image/menu_discover.png");
+    ImageIcon menuMessage = new ImageIcon("front/instagram_clone/image/menu_message.png");
+    ImageIcon menuNotice = new ImageIcon("front/instagram_clone/image/menu_notice.png");
+    ImageIcon menuUpload = new ImageIcon("front/instagram_clone/image/menu_upload.png");
+    ImageIcon menuProfil = new ImageIcon("front/instagram_clone/image/harry_potter_profil_circle.png");
+    ImageIcon menuMore = new ImageIcon("front/instagram_clone/image/menu_more.png");
 
-    ImageIcon menuGrayHome = new ImageIcon("image/menu_gray_home.png");
-    ImageIcon menuGraySearch = new ImageIcon("image/menu_gray_search.png");
-    ImageIcon menuGrayDiscover = new ImageIcon("image/menu_gray_discover.png");
-    ImageIcon menuGrayMessage = new ImageIcon("image/menu_gray_message.png");
-    ImageIcon menuGrayNotice = new ImageIcon("image/menu_gray_notice.png");
-    ImageIcon menuGrayUpload = new ImageIcon("image/menu_gray_upload.png");
-    ImageIcon menuGrayMore = new ImageIcon("image/menu_gray_more.png");
+    ImageIcon menuGrayHome = new ImageIcon("front/instagram_clone/image/menu_gray_home.png");
+    ImageIcon menuGraySearch = new ImageIcon("front/instagram_clone/image/menu_gray_search.png");
+    ImageIcon menuGrayDiscover = new ImageIcon("front/instagram_clone/image/menu_gray_discover.png");
+    ImageIcon menuGrayMessage = new ImageIcon("front/instagram_clone/image/menu_gray_message.png");
+    ImageIcon menuGrayNotice = new ImageIcon("front/instagram_clone/image/menu_gray_notice.png");
+    ImageIcon menuGrayUpload = new ImageIcon("front/instagram_clone/image/menu_gray_upload.png");
+    ImageIcon menuGrayMore = new ImageIcon("front/instagram_clone/image/menu_gray_more.png");
 
     JButton bt_menuLogo;
     JButton bt_menuHome;
