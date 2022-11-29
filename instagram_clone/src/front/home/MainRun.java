be9f6b218114bfab647dabc35721ca77c0b61544
JIYOUNG PARK <ziy913@naver.com>
2022-11-29 19:31:43 +0900

    회원가입 화면 구현

--- instagram_clone/src/front/home/MainRun.java
+++ instagram_clone/src/front/home/MainRun.java
@@ -4,11 +4,11 @@ import javax.swing.*;
 
 public class MainRun {
     public static void main(String[] args) {
-//        Login login = new Login();
-//        login.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        login.setVisible(true);
-        Home home = new Home();
-        home.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        home.setVisible(true);
+        Login login = new Login();
+        login.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        login.setVisible(true);
+//        Home home = new Home();
+//        home.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        home.setVisible(true);
     }
 }
